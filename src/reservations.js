ea279a9a408e9174d017bb73a05bed665624328a
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2021-02-10 22:03:24 +0200

    add styles to the reservations page

--- src/reservations.js
+++ src/reservations.js
@@ -1,3 +1,5 @@
+import './reservations.css';
+
 export default function reservations() {
   const header = document.createElement('h1');
   const name = document.createElement('input');
@@ -7,6 +9,7 @@ export default function reservations() {
   const submit = document.createElement('input');
   const form = document.createElement('form');
   const formWrapper = document.createElement('div');
+  const br = document.createElement('br');
 
   name.setAttribute('type', 'text');
   name.setAttribute('placeholder', 'Full Names');
@@ -20,9 +23,14 @@ export default function reservations() {
   submit.setAttribute('value', 'Reserve Table');
 
   form.appendChild(name);
+  form.appendChild(br);
+  form.appendChild(br);
   form.appendChild(email);
+  form.appendChild(br);
   form.appendChild(nberOfPeople);
+  form.appendChild(br);
   form.appendChild(date);
+  form.appendChild(br);
   form.appendChild(submit);
   formWrapper.appendChild(header);
   formWrapper.appendChild(form);
